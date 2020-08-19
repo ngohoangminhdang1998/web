58d055de20e067c51ca4342114d901e10cbe9403
ngohoangminhdang1998 <ngohoangminhdang63@gmail.com>
2020-08-19 17:50:16 +0700

    commit footer code

--- Frontend/website1/src/components/Footer/Footer.js
+++ Frontend/website1/src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
                     </div>
                 </div>
                 <div className="Footer-website-information container-fluid text-white">
-                    <div className="container d-flex justify-content-between">
+                    <div className="container d-flex justify-content-between pt-2 pb-2">
                         <div className="FIND A RESIDENCE">
                             <h6>FIND A RESIDENCE</h6>
                             <ul> <li><a href="">By Property</a></li>
@@ -61,8 +61,43 @@ class Footer extends Component {
                             </ul>
                         </div>
                     </div>
+                    <div className="Footer-asr d-flex justify-content-between">
+                        <a href="" className="pb-0"><img src="https://www.the-ascott.com/content/dam/tal/media/images/common-ascott/brand-logos/logo-asr-white.png.transform/ascott-lowres/image.png" height="26px" width="150px"/></a>
+                        <a href=""><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/ascott/logo-ascott-alt-en.png.transform/ascott-lowres/image.png" height="80px" width="100px"/></a>
+                        <a href="" className="align-logo"><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/citadines/logo-citadines-white-alt-en.png.transform/ascott-lowres/image.png" height="40px" width="100px"/></a>
+                        <a href="" className="align-logo"><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/citadines/citadines-connect-white-logo.png.transform/ascott-lowres/image.png" height="40px" width="100px"/></a>
+                        <a href=""><img src="https://www.the-ascott.com/content/dam/tal/media/images/common-ascott/brand-logos/somerset/logo-somerset-alt-en.svg" height="80px" width="150px"/></a>
+                        <a href="" className="align-logo"><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/crest/logo-thecrestcollection-alt-en.png.transform/ascott-lowres/image.png" height="40px" width="150px"/></a>
+                        <a href="" className="align-logo"><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/lyf/logo-lyf-alt-en.png.transform/ascott-lowres/image.png" height="40px" width="50px"/></a>
+                    </div>
+                    <hr/>
+                    <div className="Footer-quest d-flex justify-content-center">
+                        <a href=""><img src="https://www.the-ascott.com/content/dam/tal/media/images/logo/footer-logo/quest_footer_logo.png.transform/ascott-lowres/image.png" width="100px" height="80px"/></a>
+                        <a href=""><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/tauzia/tauzia-white-logo.png.transform/ascott-lowres/image.png" width="100px" height="80px"/></a>
+                    </div>
+                </div>
+                <div className="Footer-bottom container">
+                    <div className="row">
+                        <div className="col-md-1">
+                        <a href=""><img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/brands/ascottlimited/logo-theascottlimited.svg" width="100px" height="80px"/></a>
+                        </div>
+                        <div className="Footer-cookie col-md-8">
+                            <ul>
+                                <li><a href="">Terms of Uses</a></li>
+                                <li><a href="">Privacy Policy</a></li>
+                                <li><a href="">Cookie Preferences</a></li>
+                                <li><a href="">Guarantee and Cancellation Policy</a></li>
+                            </ul>
+                            <p><i className="far fa-copyright"></i> 2020 The Ascott Limited. ALl rights reserved</p>
+                        </div>
+                        <div className="col-md-1 col-md-offset-2">
+                            <img src="https://www.the-ascott.com/content/dam/tal/common/assets/logos/logo-capitaland.svg" width="100px" height="80px"/>
+                        </div>
+                    </div>
+                
                 </div>
             </div>
+
         )
     }
 }
