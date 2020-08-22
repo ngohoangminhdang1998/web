5da3cf62b8063bc44594df377417e3138771fb3e
ngohoangminhdang1998 <ngohoangminhdang63@gmail.com>
2020-08-22 20:34:15 +0700

    commit code slideshow using bootstrap

--- Backend/website1/routes/index.js
+++ Backend/website1/routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'minhdang63',
+  port: 5432,
+})
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log("test")
+    console.log(err, res)
+    pool.end()
+  })
   res.render('index', { title: 'Express' });
 });
 
--- Frontend/website1/src/components/Main/Main.js
+++ Frontend/website1/src/components/Main/Main.js
@@ -1,69 +1,122 @@
-import React, { Component } from 'react'
-import Main_style from "./Main.scss";
-var slideIndex = 1;
-class Main extends Component {
-    constructor(props){
-        super(props);
-        this.state={
+// import React, { Component } from 'react'
+// import Main_style from "./Main.scss";
+// var slideIndex = 1;
+// class Main extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
 
-        }
-    }   
-      showSlides=(n)=> {
-        var i;
-        // var slides = document.getElementsByClassName("my-slide");
-        // let slides = document.getElementsByClassName("my-slide");
-        var dots = document.getElementsByClassName("dot");
-        // console.log(slides);
-        // console.log(slides.length);
-        document.getElementsByClassName('my-slide')[0].style.display = 'block';
-        // document.getElementsByClassName('my-slide')[0].style.setProperty("display", "block")
-        //  if (n > slides.length) {slideIndex = 1} ;   
-        //  if (n < 1) {slideIndex = slides.length};
-        //  for (i = 0; i < slides.length; i++) {   
-        //      slides[i].style.display = "none";  
-        //    }
-        //    for (i = 0; i < dots.length; i++) {
-        //       dots[i].className = dots[i].className.replace(" active", "");
-        //   }
-        //    slides[slideIndex-1].style.display = "block";
-        //    dots[slideIndex-1].className += " active";
-      }
-    //   plusSlides = (n) => {
-    //     this.showSlides(slideIndex += n);
-    //   }
-    //   currentSlide = (n) => {
-    //     this.showSlides(slideIndex = n);
-    //   }
-    render() {
-    this.showSlides(slideIndex);
-        return (
-            <div className="main-content">
-                <div className="slide-show1 container-fluid">  
-                    <div className="my-slide">
-                        <div className="numbertext">1/3</div>
-                        <img src="https://www.the-ascott.com/content/dam/tal/media/images/homepage-banner/homepage-banner/2ascott_always_our_best_banner_1902x782.jpg.transform/ascott-highres/image.jpg" width="100%" />
-                        <div class="text">caption text</div>
-                    </div>
-                    {/* <div className="my-slide">
-                        <div className="numbertext">2/3</div>
-                        <img src="https://www.the-ascott.com/content/dam/tal/media/images/campaigns/work-in-residence/work-in-residence-homepage-2.jpg.transform/ascott-highres/image.jpg" width="100%" />
-                        <div class="text">caption text</div>
-                    </div>
-                    <div className="my-slide">
-                        <div className="numbertext">3/3</div>
-                        <img src="https://www.the-ascott.com/content/dam/tal/media/images/brands/The-Ascott/Ascott-Cares/1920x782_v3.png.transform/ascott-highres/image.png" width="100%" />
-                        <div class="text">caption text</div>
-                    </div> */}
-                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                </div>
-                <div className="dot-button-group">
-                    <span class="dot" onclick="currentSlide(1)"></span>
-                    <span class="dot" onclick="currentSlide(2)"></span>
-                    <span class="dot" onclick="currentSlide(3)"></span>
-                </div>
+//         }
+//     }
+//     showSlides = (n) => {
+//         var i;
+//          var slides = document.getElementsByClassName("my-slide");
+//          var dots = document.getElementsByClassName("dot");
+//         // let slides = document.getElementsByClassName("my-slide");
+//          console.log(slides);
+//          console.log(slides.length);
+//             // document.getElementsByClassName('my-slide')[0].style.setProperty("display", "block")
+//         //   document.getElementsByClassName('my-slide')[0].style.display = 'block';
+
+//          if (n > slides.length) {slideIndex = 1} ;   
+//          if (n < 1) {slideIndex = slides.length};
+//          for (i = 0; i < slides.length; i++) {   
+//             //  slides[i].style.display = "none";  
+//            }
+//            for (i = 0; i < dots.length; i++) {
+//               dots[i].className = dots[i].className.replace(" active", "");
+//           }
+//         //   slides[slideIndex-1].style.display = "block";
+//         //    dots[slideIndex-1].className += " active";
+//     }
+//       plusSlides = (n) => {
+//         this.showSlides(slideIndex += n);
+//       }
+//       currentSlide = (n) => {
+//         this.showSlides(slideIndex = n);
+//       }
+//     render() {
+//         this.showSlides(slideIndex);
+//         return (
+//             <div className="main-content">
+//                 <div className="slide-show1 container-fluid">
+//                     <div className="my-slide">
+//                         <div className="numbertext">1/3</div>
+//                         <img src="https://www.the-ascott.com/content/dam/tal/media/images/homepage-banner/homepage-banner/2ascott_always_our_best_banner_1902x782.jpg.transform/ascott-highres/image.jpg" width="100%" />
+//                         <div class="text">caption text</div>
+//                     </div>
+//                      <div className="my-slide">
+//                         <div className="numbertext">2/3</div>
+//                         <img src="https://www.the-ascott.com/content/dam/tal/media/images/campaigns/work-in-residence/work-in-residence-homepage-2.jpg.transform/ascott-highres/image.jpg" width="100%" />
+//                         <div class="text">caption text</div>
+//                     </div>
+//                     <div className="my-slide">
+//                         <div className="numbertext">3/3</div>
+//                         <img src="https://www.the-ascott.com/content/dam/tal/media/images/brands/The-Ascott/Ascott-Cares/1920x782_v3.png.transform/ascott-highres/image.png" width="100%" />
+//                         <div class="text">caption text</div>
+//                     </div> 
+//                     <a class="prev" onclick={()=>this.plusSlides(-1)}>&#10094;</a>
+//                     <a class="next" onclick={()=>this.plusSlides(1)}>&#10095;</a>
+//                 </div>
+//                 <div className="dot-button-group">
+//                     <span class="dot" onclick={()=>this.currentSlide(1)}></span>
+//                     <span class="dot" onclick={()=>this.currentSlide(2)}></span>
+//                     <span class="dot" onclick={()=>this.currentSlide(3)}></span>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+// export default Main;
+
+
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
+            <li data-target="#carouselExampleIndicators" data-slide-to={1} />
+            <li data-target="#carouselExampleIndicators" data-slide-to={2} />
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block w-100" src="https://www.the-ascott.com/content/dam/tal/media/ascott-star-rewards-2018/buypoints/1902x782.jpg.transform/ascott-highres/image.jpg" alt="First slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>...</h5>
+                <p>...</p>
+              </div>
             </div>
-        )
-    }
+            <div className="carousel-item">
+              <img className="d-block w-100" src="https://www.the-ascott.com/content/dam/tal/media/images/campaigns/work-in-residence/work-in-residence-homepage-2.jpg.transform/ascott-highres/image.jpg" alt="Second slide" />
+
+              <div class="carousel-caption d-none d-md-block">
+                <h5>...</h5>
+                <p>...</p>
+              </div></div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="https://www.the-ascott.com/content/dam/tal/media/images/brands/The-Ascott/Ascott-Cares/1920x782_v3.png.transform/ascott-highres/image.png" alt="Third slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>...</h5>
+                <p>...</p>
+              </div>
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    )
+  }
 }
-export default Main;
+
+
